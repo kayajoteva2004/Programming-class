8d9b50cc6aba1971b2e6e00c5d9d2b58c1dfeb67
kayajoteva2004 <kayajoteva@gmail.com>
2025-12-15 00:50:19 +0200

    Add files via upload

---
+++ Coding Challenge 1.py
@@ -0,0 +1,8 @@
+print ('Welcome to the band name generator')
+
+origin = input ("Where are you from?")
+native_language = input ("What is your native language?")
+color = input ('What is the name of your favourite colors in your native language?')
+favourite_word = input ('How is your favourite word in your native language?')
+
+print (color, favourite_word)
---
+++ Coding Challenge 2.py
@@ -0,0 +1,11 @@
+bill = float (input ("How much is the bill? "))
+tip = float (input ("What percentage tip you will leave? "))
+total = bill + (bill * tip / 100)
+print (f"You have to leave {total} money on the table.")
+
+
+
+
+
+
+
